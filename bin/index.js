61e6da8434e57dd1f4a5e1bf67a1239fa1c4069e
Daniel Schneider <daniel.schneider@eramux.com>
2022-01-24 22:12:33 +0100

    chore: revert folder renaming

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const ASSETS_PATH = path.join(__dirname, "../assets");
-const COMPONENTS_PATH = path.join(__dirname, "../src/lib-components");
-const INDEX_PATH = path.join(__dirname, "../src/lib-components/index.ts");
+const COMPONENTS_PATH = path.join(__dirname, "../src/components");
+const INDEX_PATH = path.join(__dirname, "../src/components/index.ts");
 
 module.exports = { ASSETS_PATH, COMPONENTS_PATH, INDEX_PATH };
--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,7 +1,7 @@
 import _Vue, { PluginFunction } from "vue";
 
 // Import vue components
-import * as components from "@/lib-components/index";
+import * as components from "@/components/index";
 
 // install function executed by Vue.use()
 const install: PluginFunction<any> = function installPhosphorVue(
@@ -17,4 +17,4 @@ export default install;
 
 // To allow individual component use, export components
 // each can be registered via Vue.component()
-export * from "@/lib-components/index";
+export * from "@/components/index";
