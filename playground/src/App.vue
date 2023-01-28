036f8bd1f2a45a1d4f600b1c0d89ff3c67cebb03
Daniel Schneider <daniel.schneider@eramux.com>
2023-01-28 15:03:28 +0100

    feat: add playground and issue templates

---
+++ playground/src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+
+    <div class="max-w-screen-lg w-full mx-auto grid grid-cols-10 gap-8 py-8">
+        <h1 class="col-span-full text-4xl ">
+            Phosphor Icons Vue 3
+        </h1>
+        <div  v-for="icon in icons" class="aspect-square flex items-center justify-center rounded-3xl bg-gray-50 text-gray-600">
+            <component :is="icon" size="32"/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import * as icons from "@phosphor-icons/vue"
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ playground/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles/tailwind.css'
+
+createApp(App).mount('#app')
