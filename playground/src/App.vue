dc7e25dc2c407db54abb777ea8f973c2c060fdad
Daniel Schneider <daniel.schneider@eramux.com>
2023-01-29 16:45:27 +0100

    feat: improve playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="max-w-screen-lg w-full mx-auto grid grid-cols-10 gap-8 p-8">
-    <h1 class="col-span-full text-4xl">Phosphor Icons Vue 3</h1>
+    <h1 class="col-span-full text-4xl flex items-center">
+      <PhPhosphorLogo class="mr-2" /> Phosphor Icons Vue 3
+    </h1>
     <div class="col-span-full text-2xl text-gray-400">
       Total icon count: {{ total }}
     </div>
@@ -22,6 +24,7 @@
 </template>
 
 <script setup>
+import { PhPhosphorLogo } from '@phosphor-icons/vue';
 import * as icons from '@phosphor-icons/vue';
 
 let total = Object.keys(icons).length;
